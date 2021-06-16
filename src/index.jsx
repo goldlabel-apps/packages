b7b7f5070f0eebe4fc7c3fbe8ac397de132b3b2c
listingslab <listingslab@gmail.com>
2021-06-16 10:17:28 +1000

    env

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
+import pJSON from '../package.json'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import reportWebVitals from './reportWebVitals'
+
+console.log( `${process.env.REACT_APP_APP} ${pJSON.version} (${process.env.REACT_APP_ENV})` )
 
 ReactDOM.render(
   <React.StrictMode>
