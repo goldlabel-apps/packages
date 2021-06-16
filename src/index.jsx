d1778f56cab757cfaac0a515b9d9037ec6aa4626
listingslab <listingslab@gmail.com>
2021-06-16 11:05:30 +1000

    adds "@material-ui/core": "^4.11.4",

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import pJSON from '../package.json'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import reportWebVitals from './reportWebVitals'
 import { SkipIntro } from './packages'
 
 console.log( `${process.env.REACT_APP_APP} ${pJSON.version} (${process.env.REACT_APP_ENV})` )
@@ -16,5 +15,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('skip-intro')
 )
-
-reportWebVitals()
